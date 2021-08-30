2c261b595f45618a297e6b6b8a60d956d3ccffdd
allanncruz <contato@allancruz.com.br>
2021-08-30 09:45:26 -0300

    feat: config import and order

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,16 @@
+import api from 'axios';
 import React from 'react';
-import { useSelector } from 'react-redux'
-import api  from 'axios';
+import { useSelector } from 'react-redux';
+
 import Header from '../../components/Header';
 import { Container, Title } from './styles';
 import User from './User';
 
 export default function Main() {
   return (
-      <Container>
-          <Title>Eslint Import</Title>
-          <User />
-      </Container>
-  )
+    <Container>
+      <Title>Eslint Import</Title>
+      <User />
+    </Container>
+  );
 }
