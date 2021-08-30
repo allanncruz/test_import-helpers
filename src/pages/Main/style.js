0ddf2a81cbfc0650b7ee9f8270e07da024e706b1
allanncruz <contato@allancruz.com.br>
2021-08-30 09:03:12 -0300

    feat: config styld component

---
+++ src/pages/Main/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #333;
+`;
+
+export const Title = styled.h1`
+  color: #fff;
+  font-size: 30px;
+`;
+
